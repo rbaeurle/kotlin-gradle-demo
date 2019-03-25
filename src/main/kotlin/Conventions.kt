954104d028a251eabbd635de4d8094ad511f834d
Rolf Bäurle <rolf.baeurle@novatec-gmbh.de>
2019-03-25 08:27:16 +0100

    implement Conventions

--- src/main/kotlin/Conventions.kt
+++ src/main/kotlin/Conventions.kt
@@ -10,10 +10,9 @@ fun main(args: Array<String>) {
     println( checkInRange2(MyDate(2002,9,15),
         MyDate(1967,10,26),
         MyDate(1970,5,14)))
-    //d : DateRange2 = MyDate(2019,3,19)..MyDate(2019,3,22)
-//    for ( d in  MyDate(2019,3,19)..MyDate(2019,3,22)) {
-//        println(d)
-//    }
+    for ( d in  MyDate(2019,3,19)..MyDate(2019,3,22)) {
+        println(d)
+    }
 }
 
 //
@@ -37,8 +36,11 @@ fun compare(date1: MyDate, date2: MyDate) = date1 < date2
 //
 // In range
 //
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
-    operator fun contains(d: MyDate) = start <= d && d <= endInclusive
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+
+    override operator fun contains(value: MyDate) = start <= value && value <= endInclusive
+
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
 }
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
@@ -48,9 +50,9 @@ fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
 //
 // rangeTo
 //
-operator fun MyDate.rangeTo(other: MyDate) = DateRange2(this, other)
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
-class DateRange2(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
+//class DateRange2(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
 
 fun checkInRange2(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in first..last
